8d5ec77c93ad459e6de92886136bcbe3e3951aa2
dnaals <swm8793@naver.com>
2024-01-31 12:50:31 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log(Notification.permission);
+    Notification.requestPermission().then();
+  }, [])
 
-  Notification.requestPermission().then();
+
+
   function msg() {
     navigator.serviceWorker.controller.postMessage({
       message: "Hello, service worker~~~"
