3c4beead4330ed6813b39af4b463995c0fe563d3
dnaals <swm8793@naver.com>
2024-02-01 12:26:45 +0900

    47

--- src/service-worker.js
+++ src/service-worker.js
@@ -78,9 +78,9 @@ registerRoute(
 self.addEventListener('message', (event) => {
     const option = {
         body: event.data.message,
-        icon: './img/img.png',        //제목옆에 아이콘
-        image: './img/img.png',       //내용에 대한 썸네일
-        badge: './img/img.png',
+        icon: './img/img1.png',        //제목옆에 아이콘
+        image: './img/img2.png',       //내용에 대한 썸네일
+        badge: './img/img3.png',
         vibrate: [200, 100, 300],
         actions: [
             { action: 'open', title: '자세히보기' },
