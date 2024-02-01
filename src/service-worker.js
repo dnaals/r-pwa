fdf505b0a3374419878da791420d17925b0cd67a
dnaals <swm8793@naver.com>
2024-02-01 12:17:37 +0900

    14

--- src/service-worker.js
+++ src/service-worker.js
@@ -26,39 +26,39 @@
 // // https://developers.google.com/web/fundamentals/architecture/app-shell
 // const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 // registerRoute(
-//   // Return false to exempt requests from being fulfilled by index.html.
-//   ({ request, url }) => {
-//     // If this isn't a navigation, skip.
-//     if (request.mode !== 'navigate') {
-//       return false;
-//     } // If this is a URL that starts with /_, skip.
+//     // Return false to exempt requests from being fulfilled by index.html.
+//     ({ request, url }) => {
+//         // If this isn't a navigation, skip.
+//         if (request.mode !== 'navigate') {
+//             return false;
+//         } // If this is a URL that starts with /_, skip.
 
-//     if (url.pathname.startsWith('/_')) {
-//       return false;
-//     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+//         if (url.pathname.startsWith('/_')) {
+//             return false;
+//         } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
-//     if (url.pathname.match(fileExtensionRegexp)) {
-//       return false;
-//     } // Return true to signal that we want to use the handler.
+//         if (url.pathname.match(fileExtensionRegexp)) {
+//             return false;
+//         } // Return true to signal that we want to use the handler.
 
-//     return true;
-//   },
-//   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+//         return true;
+//     },
+//     createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 // );
 
 // // An example runtime caching route for requests that aren't handled by the
 // // precache, in this case same-origin .png requests like those from in public/
 // registerRoute(
-//   // Add in any other file extensions or routing criteria as needed.
-//   ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-//   new StaleWhileRevalidate({
-//     cacheName: 'images',
-//     plugins: [
-//       // Ensure that once this runtime cache reaches a maximum size the
-//       // least-recently used images are removed.
-//       new ExpirationPlugin({ maxEntries: 50 }),
-//     ],
-//   })
+//     // Add in any other file extensions or routing criteria as needed.
+//     ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+//     new StaleWhileRevalidate({
+//         cacheName: 'images',
+//         plugins: [
+//             // Ensure that once this runtime cache reaches a maximum size the
+//             // least-recently used images are removed.
+//             new ExpirationPlugin({ maxEntries: 50 }),
+//         ],
+//     })
 // );
 
 // // This allows the web app to trigger skipWaiting via
@@ -76,33 +76,33 @@
 // //   })
 // // })
 // self.addEventListener('message', (event) => {
-//   const option = {
-//     body: event.data.message,
-//     icon: './img/img1.png',        //제목옆에 아이콘
-//     image: './img/img2.png',       //내용에 대한 썸네일
-//     badge: './img/img3.png',
-//     vibrate: [200, 100, 300],
-//     actions: [
-//       { action: 'open', title: '자세히보기' },
-//       { action: 'close', title: '닫기' }
-//     ]
-//   }
+//     const option = {
+//         body: event.data.message,
+//         icon: './img/img1.png',        //제목옆에 아이콘
+//         image: './img/img2.png',       //내용에 대한 썸네일
+//         badge: './img/img3.png',
+//         vibrate: [200, 100, 300],
+//         actions: [
+//             { action: 'open', title: '자세히보기' },
+//             { action: 'close', title: '닫기' }
+//         ]
+//     }
 
-//   self.registration.showNotification('title', option);
+//     self.registration.showNotification('title', option);
 // })
 
 
 
-// self.addEventListener('notificationclick', (event) => {
-//   event.waitUntil(
-//     self.clients.matchAll().then(function (clientList) {
-//       if (event.action == 'open') {
-//         return self.clients.openWindow('https://naver.com');
-//       } else if (event.action == 'close') {
-//         return event.notification.close();
-//       }
-//     })
-//   )
-// })
+self.addEventListener('notificationclick', (event) => {
+    event.waitUntil(
+        self.clients.matchAll().then(function (clientList) {
+            if (event.action == 'open') {
+                return self.clients.openWindow('https://naver.com');
+            } else if (event.action == 'close') {
+                return event.notification.close();
+            }
+        })
+    )
+})
 
 // // Any other custom service worker logic can go here.
